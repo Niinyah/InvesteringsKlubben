5be40fa27de8c12ab875ccd0a972a36dee9cbfe1
Uller <fullersted@gmail.com>
2025-04-30 15:03:48 +0200

    added empty method bodies

--- src/Repository/TransactionRepository.java
+++ src/Repository/TransactionRepository.java
@@ -1,4 +1,17 @@
 package Repository;
 
-public class TransactionRepository {
+import Model.TransactionLine;
+
+import java.util.List;
+
+public class TransactionRepository implements iTransactionRepository {
+    @Override
+    public List<TransactionLine> getTransactions(){
+        return List.of();
+    }
+
+    @Override
+    public void writeTransactionLine(TransactionLine transactionLine){
+
+    }
 }

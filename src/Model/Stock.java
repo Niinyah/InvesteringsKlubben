d9000aac0c73516ed7911af5b0d49ecf9a638e2d
Niinyah <petermarbell@gmail.com>
2025-04-30 12:45:42 +0200

    Stock version 1

--- src/Model/Stock.java
+++ src/Model/Stock.java
@@ -1,4 +1,64 @@
 package Model;
 
+import java.time.LocalDate;
+
 public class Stock {
+    private String ticker;
+    private String name;
+    private String sector;
+    private double price;
+    private String currency;
+    private String rating;
+    private double dividendYield;
+    private String market;
+    private LocalDate lastUpdated;
+
+
+    public Stock(String ticker, String name, String sector, double price, String currency, String rating, double dividendYield, String market, LocalDate lastUpdated) {
+        this.ticker = ticker;
+        this.name = name;
+        this.sector = sector;
+        this.price = price;
+        this.currency = currency;
+        this.rating = rating;
+        this.dividendYield = dividendYield;
+        this.market = market;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public double getDividendYield() {
+        return dividendYield;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public LocalDate getLastUpdated() {
+        return lastUpdated;
+    }
 }

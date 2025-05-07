48b9c34cc5ddbf7a0be9645734b2e08776aa8195
AdamHoppe98 <adamhoppe98@gmail.com>
2025-05-07 08:51:41 +0200

    fix

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main {
         IStockMarketRepository stockMarketRepository = new StockMarketRepository();
         IUserRepository userRepository = new UserRepository();
         IStockMarketService stockMarketService = new StockMarketService(stockMarketRepository);
-        Service.ITransactionService transactionService = new TransactionService(transactionRepository, stockMarketService);
-        Service.IUserService userService = new UserService(userRepository);
+        ITransactionService transactionService = new TransactionService(transactionRepository, stockMarketService);
+        IUserService userService = new UserService(userRepository);
         IPortfolioService portfolioService = new PortfolioService(transactionService, stockMarketService, userService);
         TerminalUserInterface terminalUserInterface = new TerminalUserInterface();
         Controller controller = new Controller(portfolioService, stockMarketService, transactionService, userService, terminalUserInterface);
